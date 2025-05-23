9f474f740306d0cbd11e320963cb2c9e009bb439
Lyubomir Radkov <lyubomirradkov@gmail.com>
2025-05-23 13:57:46 +0000

    Add Google Chat integration and utility functions for alerts

---
+++ airflow-alerts/src/airflow_alerts/google_chat.py
@@ -0,0 +1,291 @@
+import json
+from datetime import datetime
+from datetime import time as dt_time
+from datetime import timedelta
+from enum import Enum
+
+from airflow.hooks.base import BaseHook
+
+from .utils import (
+    _build_task_log_url,
+    _build_task_run_id,
+    _ensure_https,
+    _get_dag_run_identifiers,
+    _get_redis_client,
+    _send_post_request,
+    _validate_url,
+)
+
+
+class STATUS_COLORS(Enum):
+    """Enum for Google Chat message colors."""
+
+    success = {"red": 0.8, "green": 1, "blue": 0.8}
+    failure = {"red": 1, "green": 0.8, "blue": 0.8}
+    default = {"red": 0.95, "green": 0.95, "blue": 0.95}
+    retry = {"red": 1, "green": 0.9, "blue": 0.7}
+    skipped = {"red": 0.9, "green": 0.8, "blue": 1}
+    sla_miss = {"red": 1, "green": 0.85, "blue": 0.85}
+
+
+def _construct_webhook_url(connection_id: str, run_id: str = ""):
+    """
+    Constructs the webhook URL for the specified connection ID.
+    Args:
+        connection_id (str): The connection ID.
+        run_id (str): The optional thread reference. TODO fix this
+    Returns:
+        str: The constructed URL.
+    """
+    gchat_connection = BaseHook.get_connection(connection_id)
+    # Reference for Google Chat API:
+    # https://developers.google.com/workspace/chat/api/reference/rest/v1/spaces.messages/create
+    # MessageReplyOption details:
+    # https://developers.google.com/workspace/chat/api/reference/rest/v1/spaces.messages/create#MessageReplyOption
+    thread_ref = (
+        f"&thread_key={run_id}&messageReplyOption=REPLY_MESSAGE_FALLBACK_TO_NEW_THREAD"
+    )
+    full_url = f"{gchat_connection.password}{thread_ref}"
+    return full_url
+
+
+def _send_message(dag_id, run_id, connection_id, message_body):
+    run_id = _build_task_run_id(dag_id, run_id)
+    full_url = _ensure_https(_construct_webhook_url(connection_id, run_id))
+    if not _validate_url(full_url):
+        print(f"Invalid URL: {full_url}")
+        return
+    print("Sending message to Google Chat")
+    _send_post_request(message_body, full_url)
+
+
+def basic_alert(
+    connection_id: str, message_body: str, redis_conn_id: str, delay: dt_time = None
+):
+    """
+    Sends a basic alert to Google Chat.
+    Useful to send small informative messages in working hours to stakeholders.
+    Args:
+        connection_id (str): The connection ID.
+        message_body (str): The message body.
+        redis_conn_id (str): The Redis connection ID.
+        delay (dt_time): The time to delay the alert. If None, the alert is sent immediately.
+    """
+
+    def basic_alert_inner(context):
+        dag_id, run_id = _get_dag_run_identifiers(context)
+        if delay is not None:
+            redis_conn = _get_redis_client(redis_conn_id)
+            now = datetime.now()
+            target = now.replace(
+                hour=delay.hour, minute=delay.minute, second=delay.second, microsecond=0
+            )
+            if target <= now:
+                target += timedelta(days=1)
+            # Prepare alert data
+            alert_data = {
+                "dag_id": dag_id,
+                "run_id": run_id,
+                "connection_id": connection_id,
+                "message_body": message_body,
+            }
+            # Store in Redis sorted set with target timestamp as score
+            redis_conn.zadd(
+                "scheduled_alerts", {json.dumps(alert_data): target.timestamp()}
+            )
+            print(f"Alert scheduled for {target.strftime('%Y-%m-%d %H:%M:%S')}")
+        else:
+            _send_message(dag_id, run_id, connection_id, message_body)
+
+    return basic_alert_inner
+
+
+def task_success_alert(connection_id: str):
+    """
+    Sends a task success alert to Google Chat.
+    Args:
+        connection_id (str): The connection ID.
+    """
+
+    def task_success_alert_inner(context):
+        task_instance = context.get("task_instance")
+        task_log_url = _build_task_log_url(task_instance)
+        # For more details on the message format, refer to the Google Chat API documentation:
+        # https://developers.google.com/chat/api/guides/message-formats/cards
+        # https://developers.google.com/workspace/chat/api/reference/rest/v1/cards
+        message_body = {
+            "cardsV2": [
+                {
+                    "cardId": "airflow-task-success",
+                    "card": {
+                        "header": {
+                            "title": "✅ Task completed successfully!",
+                        },
+                        "sections": [
+                            {
+                                "widgets": [
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Task Name",
+                                            "text": f"<b>{task_instance.task.task_display_name}</b>",
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Task Description",
+                                            "text": task_instance.task.doc_md,
+                                            "wrapText": True,
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "DAG ID",
+                                            "text": str(task_instance.dag_id),
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Hostname",
+                                            "text": str(task_instance.hostname),
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Execution Date",
+                                            "text": task_instance.start_date.strftime(
+                                                "%Y-%m-%d %H:%M:%S"
+                                            ),
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Execution number / Max executions",
+                                            "text": f"{task_instance.try_number} / {task_instance.max_tries + 1}",
+                                        }
+                                    },
+                                    {
+                                        "buttonList": {
+                                            "buttons": [
+                                                {
+                                                    "text": "<b>View Logs</b>",
+                                                    "color": STATUS_COLORS.success.value,
+                                                    "onClick": {
+                                                        "openLink": {
+                                                            "url": task_log_url
+                                                        }
+                                                    },
+                                                }
+                                            ]
+                                        }
+                                    },
+                                ]
+                            }
+                        ],
+                    },
+                }
+            ]
+        }
+        _send_message(
+            dag_id=task_instance.dag_id,
+            run_id=task_instance.run_id,
+            message_body=message_body,
+            connection_id=connection_id,
+        )
+
+    return task_success_alert_inner
+
+
+def task_failure_alert(connection_id: str):
+    """
+    Sends a task failure alert to Google Chat.
+    Args:
+        connection_id (str): The connection ID.
+    """
+
+    def task_failure_alert_inner(context):
+        task_instance = context.get("task_instance")
+        task_log_url = _build_task_log_url(task_instance)
+        # For more details on the message format, refer to the Google Chat API documentation:
+        # https://developers.google.com/chat/api/guides/message-formats/cards
+        # https://developers.google.com/workspace/chat/api/reference/rest/v1/cards
+        message_body = {
+            "cardsV2": [
+                {
+                    "cardId": "airflow-task-success",
+                    "card": {
+                        "header": {
+                            "title": "❌ Task failed!",
+                        },
+                        "sections": [
+                            {
+                                "widgets": [
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Task Name",
+                                            "text": f"<b>{task_instance.task.task_display_name}</b>",
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Task Description",
+                                            "text": task_instance.task.doc_md,
+                                            "wrapText": True,
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "DAG ID",
+                                            "text": str(task_instance.dag_id),
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Hostname",
+                                            "text": str(task_instance.hostname),
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Execution Date",
+                                            "text": task_instance.start_date.strftime(
+                                                "%Y-%m-%d %H:%M:%S"
+                                            ),
+                                        }
+                                    },
+                                    {
+                                        "decoratedText": {
+                                            "topLabel": "Execution number / Max executions",
+                                            "text": f"{task_instance.try_number} / {task_instance.max_tries + 1}",
+                                        }
+                                    },
+                                    {
+                                        "buttonList": {
+                                            "buttons": [
+                                                {
+                                                    "text": "<b>View Logs</b>",
+                                                    "color": STATUS_COLORS.failure.value,
+                                                    "onClick": {
+                                                        "openLink": {
+                                                            "url": task_log_url
+                                                        }
+                                                    },
+                                                }
+                                            ]
+                                        }
+                                    },
+                                ]
+                            }
+                        ],
+                    },
+                }
+            ]
+        }
+
+        _send_message(
+            dag_id=task_instance.dag_id,
+            run_id=task_instance.run_id,
+            message_body=message_body,
+            connection_id=connection_id,
+        )
+
+    return task_failure_alert_inner
---
+++ airflow-alerts/src/airflow_alerts/utils.py
@@ -0,0 +1,113 @@
+import os
+from urllib.parse import urlparse
+
+import redis
+import requests
+from airflow.hooks.base import BaseHook
+
+
+def _ensure_https(full_url: str) -> str:
+    """
+    Ensures that the given URL uses HTTPS.
+    Args:
+        full_url (str): The URL to check.
+    Returns:
+        str: The URL with HTTPS scheme if it was not already.
+    """
+    parsed_url = urlparse(full_url)
+    if parsed_url.scheme != "https":
+        full_url = parsed_url._replace(scheme="https").geturl()
+    return full_url
+
+
+def _validate_url(full_url: str) -> bool:
+    """
+    Validates a given URL.
+    Args:
+        full_url (str): The URL to validate.
+    Returns:
+        bool: True if the URL is valid, False otherwise.
+    """
+    try:
+        result = urlparse(full_url)
+        return all([result.scheme, result.netloc])
+    except AttributeError:
+        return False
+
+
+def _send_post_request(message_body, full_url):
+    """
+    Sends a POST request to the specified URL with the given message body.
+    Args:
+        message_body (dict): The message body to send.
+        full_url (str): The URL to send the request to.
+    """
+    r = requests.post(
+        url=full_url,
+        json=message_body,
+        headers={"Content-type": "application/json"},
+    )
+    if r.status_code != 200:
+        print(f"Error: {r.status_code} - {r.text}")
+        print("Failed to send message.")
+    else:
+        print("Message sent successfully!")
+
+
+def _build_task_log_url(task_instance) -> str:
+    """
+    Constructs the task log URL for the given task instance.
+    Args:
+        task_instance (TaskInstance): The task instance.
+    Returns:
+        str: The constructed task log URL.
+    """
+    base_url = os.environ.get("AIRFLOW__API__BASE_URL", "http://localhost:8080")
+    if "local" not in base_url:
+        base_url = _ensure_https(base_url)
+    task_url = f"{base_url}/dags/{task_instance.dag_id}/runs/{task_instance.run_id}/tasks/{task_instance.task_id}"
+    return task_url
+
+
+def _get_redis_client(connection_id="redis_default"):
+    """
+    Returns a Redis client using Airflow connection.
+    Args:
+        connection_id (str): The Airflow connection ID for Redis.
+    Returns:
+        redis.Redis: Redis client instance.
+    """
+    conn = BaseHook.get_connection(connection_id)
+    return redis.Redis(
+        host=conn.host,
+        port=conn.port or 6379,
+        db=int(conn.schema or 0),
+        password=conn.password or None,
+    )
+
+
+def _build_task_run_id(dag_id, run_id) -> str:
+    """
+    Constructs a unique task run ID based on the context.
+    Args:
+        context (dict): The context dictionary.
+    Returns:
+        str: The constructed task run ID.
+    """
+    dag_id = str(dag_id)
+    run_id = str(run_id).replace("+", "-").replace(":", "-")
+    return f"{dag_id}-{run_id}"
+
+
+def _get_dag_run_identifiers(context) -> tuple:
+    """
+    Extracts the DAG ID and run ID from the context.
+    Args:
+        context (dict): The context dictionary.
+    Returns:
+        tuple: A tuple containing the DAG ID and run ID.
+    """
+    task_instance = context.get("task_instance")
+    dag_id = str(task_instance.dag_id)
+    run_id = str(task_instance.run_id)
+    return dag_id, run_id
